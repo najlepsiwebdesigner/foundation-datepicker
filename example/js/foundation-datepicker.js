44bdd93a9b38ee774460df6847a0455897cafe96
Peter Beňo <najlepsiwebdesigner@gmail.com>
2013-02-16 13:42:13 +0100

    - fixed issue #2
    (bad component placement if source element has margin top and
    margin-bottom set)

--- example/js/foundation-datepicker.js
+++ example/js/foundation-datepicker.js
@@ -313,19 +313,20 @@
 			this.update();
 			this.updateNavArrows();
 		},
-
+		
 		place: function(){
-						if(this.isInline) return;
-			var zIndex = parseInt(this.element.parents().filter(function() {
-							return $(this).css('z-index') != 'auto';
-						}).first().css('z-index'))+10;
-			var offset = this.component ? this.component.offset() : this.element.offset();
-			var height = this.component ? this.component.outerHeight(true) : this.element.outerHeight(true);
-			this.picker.css({
-				top: offset.top + height,
-				left: offset.left,
-				zIndex: zIndex
-			});
+		        if(this.isInline) return;
+		  var zIndex = parseInt(this.element.parents().filter(function() {
+		          return $(this).css('z-index') != 'auto';
+		        }).first().css('z-index'))+10;
+		  var textbox = this.component ? this.component : this.element;
+		  var offset = textbox.offset();
+		  var height = textbox.outerHeight() + parseInt(textbox.css('margin-top'));
+		  this.picker.css({
+		    top: offset.top + height,
+		    left: offset.left,
+		    zIndex: zIndex
+		  });
 		},
 
 		update: function(){
