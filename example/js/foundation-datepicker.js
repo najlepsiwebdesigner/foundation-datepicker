00fc1a9f7267b2753f2f96366a3536f7e5551554
lightxu <lightxuzju@gmail.com>
2013-12-30 13:02:18 +0800

    Fix foundation icon fonts 3 issue
    change prev and next icon to fi-arrow-left/right

--- example/js/foundation-datepicker.js
+++ example/js/foundation-datepicker.js
@@ -994,9 +994,9 @@
 		},
 		headTemplate: '<thead>'+
 							'<tr>'+
-								'<th class="prev"><i class="icon-chevron-left"/></th>'+
+								'<th class="prev"><i class="fi-arrow-left"/></th>'+
 								'<th colspan="5" class="date-switch"></th>'+
-								'<th class="next"><i class="icon-chevron-right"/></th>'+
+								'<th class="next"><i class="fi-arrow-right"/></th>'+
 							'</tr>'+
 						'</thead>',
 		contTemplate: '<tbody><tr><td colspan="7"></td></tr></tbody>',
