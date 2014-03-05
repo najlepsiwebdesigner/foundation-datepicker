72079f891a438f5eec51865aa5a0a277fe6f7372
Hartmut Glücker <hartgluecker@gmx.net>
2014-03-05 14:40:04 +0100

    added German localization to available languages

--- example/js/foundation-datepicker.js
+++ example/js/foundation-datepicker.js
@@ -857,6 +857,14 @@
 		    months: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
 		    monthsShort: ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"],
 		    today: "Oggi"
+		},
+		de: {
+		    days: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"],
+		    daysShort: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
+		    daysMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
+		    months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+		    monthsShort: ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
+		    today: "Heute"
 		}
 	};
 
