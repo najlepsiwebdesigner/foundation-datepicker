ce535e1f242255c564a34061cc46bcb1365bdc00
ixdi <digistilts@gmail.com>
2015-10-18 21:37:36 +0200

    Added Catalan locale

---
+++ js/locales/foundation-datepicker.ca.js
@@ -0,0 +1,15 @@
+/**
+ * Catalan translation for foundation-datepicker
+ * Sergi Cortes <digistilts@gmail.com>
+ * recomanacions de l'Institut d'Estudis Catalans - http://www.iecat.net/scb/qcs_171.pdf
+ */
+;(function($){
+	$.fn.fdatepicker.dates['ca'] = {
+		days: ["Diumenge", "Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte", "Diumenge"],
+		daysShort: ["dg.", "dl.", "dt.", "dc.", "dj.", "dv.", "ds.", "dg."],
+		daysMin: ["dg.", "dl.", "dt.", "dc.", "dj.", "dv.", "ds.", "dg."],
+		months: ["Gener", "Febrer", "Març", "Abril", "Maig", "Juny", "Juliol", "Agost", "Setembre", "Octubre", "Novembre", "Desembre"],
+		monthsShort: ["gen.", "febr.", "març", "abr.", "maig", "juny", "jul.", "ag.", "set.", "oct.", "nov.", "des."],
+		today: "Avui"
+	};
+}(jQuery));
