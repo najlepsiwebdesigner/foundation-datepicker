8135d8b34be2354107b908acf1b2ae303c0cf7b7
atouza-opentrends <31236071+atouza-opentrends@users.noreply.github.com>
2017-10-11 13:12:49 +0200

    Add files via upload

---
+++ js/locales/foundation-datepicker.oc.js
@@ -0,0 +1,19 @@
+/**
+ * Occitan translation for foundation-datepicker, language file from bootstrap-datepicker
+ * Alejandro Touza <atouza@opentrends.net>
+ */
+;(function($){
+	if($.fn.fdatepicker){
+		$.fn.fdatepicker.dates['oc'] = {
+			days: ["Dimenge", "Diluns", "Dimars", "Dimècres", "Dijòus", "Divendres", "Dissabte"],
+			daysShort: ["Dim",  "Dil", "Dmt", "Dmc", "Dij", "Div", "Dis"],
+			daysMin: ["dg", "dl", "dt", "dc", "dj", "dv", "ds"],
+			months: ["Genièr", "Febrièr", "Març", "Abril", "Mai", "Junh", "Julh", "Agóst", "Setembr", "Octobre", "Novembre", "Decembre"],
+			monthsShort: ["Gen", "Feb", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Oct", "Nov", "Dec"],
+			today: "Uèi",
+			clear: "Escafar",
+			weekStart: 1,
+			format: "dd/mm/yyyy"
+		};
+	}
+}(jQuery));
