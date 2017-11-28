9560c82581d43ea3fff0680252d8deb8a0d9e5c6
demonodojo <demonodojo@gmail.com>
2017-11-28 21:30:18 +0100

    Update foundation-datepicker.es.js

--- js/locales/foundation-datepicker.es.js
+++ js/locales/foundation-datepicker.es.js
@@ -9,6 +9,8 @@
 		daysMin: ["do", "lu", "ma", "mi", "ju", "vi", "sa", "do"],
 		months: ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"],
 		monthsShort: ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"],
-		today: "hoy"
+		today: "hoy",
+		weekStart: 1,
+		format: "dd/mm/yyyy"
 	};
 }(jQuery));
